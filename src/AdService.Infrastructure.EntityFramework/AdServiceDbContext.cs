aee13c881cf482dbca7b946816d9ba5cc9920175
Andrey <kasimovskiy.a.d@gmail.com>
2024-10-10 22:00:13 +0300

    Создание контекста для БД

---
+++ src/AdService.Infrastructure.EntityFramework/AdServiceDbContext.cs
@@ -0,0 +1,22 @@
+﻿using AdService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdService.Infrastructure.EntityFramework
+{
+	public class AdServiceDbContext : DbContext
+	{
+		public DbSet<Advertisement> Advertisements { get; set; }
+
+		public DbSet<Category> Categories { get; set; }
+
+		public DbSet<User> Users { get; set; }
+
+		public DbSet<UserContactInfo> UsersContactInfo { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+			optionsBuilder.UseNpgsql("Host=localhost;Database=adservicedb;Port=5432;Username=application_user;password=appuser");
+		}
+	}
+}
