4edb624c868c54cf3b217e847945fdf69842efac
Andrey <kasimovskiy.a.d@gmail.com>
2024-10-12 23:49:05 +0300

    Добавление базового типа для сущностей

--- src/AdService.Domain.Entities/Category.cs
+++ src/AdService.Domain.Entities/Category.cs
@@ -1,27 +1,24 @@
-﻿namespace AdService.Domain.Entities
+﻿namespace AdService.Domain.Entities;
+
+/// <summary>
+/// Категория
+/// </summary>
+public class Category : Entity
 {
 	/// <summary>
-	/// Категория
+	/// Имя категории
 	/// </summary>
-	public class Category
-	{
-		public int Id { get; set; }
+	public required string Name { get; set; }
 
-		/// <summary>
-		/// Имя категории
-		/// </summary>
-		public required string Name { get; set; }
+	/// <summary>
+	/// Описание категории
+	/// </summary>
+	public string? Description { get; set; }
 
-		/// <summary>
-		/// Описание категории
-		/// </summary>
-		public string? Description { get; set; }
+	/// <summary>
+	/// Родительская категория
+	/// </summary>
+	public Category? ParentCategory { get; set; }
 
-		/// <summary>
-		/// Родительская категория
-		/// </summary>
-		public Category? ParentCategory { get; set; }
-
-		public int? ParentCategoryId { get; set; }
-	}
+	public int? ParentCategoryId { get; set; }
 }
---
+++ src/AdService.Domain.Entities/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace AdService.Domain.Entities;
+
+/// <summary>
+/// Базовый тип для сущности
+/// </summary>
+public abstract class Entity
+{
+	/// <summary>
+	/// Идентификатор
+	/// </summary>
+	public int Id { get; set; }
+}
