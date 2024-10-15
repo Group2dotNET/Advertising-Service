3ab9a97a4200cea41019e885d7399d4a378241b2
Andrey <kasimovskiy.a.d@gmail.com>
2024-10-15 22:24:08 +0300

    Добавлен проект абсракиций для сервисов и интерфейс сервиса для работы с объявлениями

---
+++ src/AdService.Application.Abstractions/Services/IAdvertisementsService.cs
@@ -0,0 +1,8 @@
+﻿using AdService.Application.Contracts;
+
+namespace AdService.Application.Abstractions.Services;
+
+public interface IAdvertisementsService
+{
+	Task<IEnumerable<AdvertisementShortDto>> GetAllAdvertisements();
+}
