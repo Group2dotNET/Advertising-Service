cd85c28ba8365ef4ec1c1bae50bbf698daa6c0ec
Andrey <kasimovskiy.a.d@gmail.com>
2024-10-15 22:24:30 +0300

    Добавлена реализация сервиса для работы с объявлениями

---
+++ src/AdService.Application.Services/AdvertisementService.cs
@@ -0,0 +1,23 @@
+﻿using AdService.Application.Abstractions.Services;
+using AdService.Application.Contracts;
+using AdService.Domain.Abstractions.Repositories;
+
+namespace AdService.Application.Services
+{
+	public class AdvertisementService : IAdvertisementsService
+	{
+		private readonly IAdvertisementRepository _advertisementRepository;
+
+		public AdvertisementService(IAdvertisementRepository advertisementRepository)
+		{
+			_advertisementRepository = advertisementRepository;
+		}
+
+		public async Task<IEnumerable<AdvertisementShortDto>> GetAllAdvertisements()
+		{
+			var advertisements = await _advertisementRepository.GetAllAsync();
+			return advertisements.Select(a => new AdvertisementShortDto { Title = a.Title, LastPublishingDate = a.UpdateDate ?? a.CreateDate }) 
+				?? Array.Empty<AdvertisementShortDto>();
+		}
+	}
+}
