7109452f823546cf588e1c4d90aa2cc147b8131f
Andrey <kasimovskiy.a.d@gmail.com>
2024-12-03 02:12:48 +0300

    Создан хост и реализован репозиторий для объявлений

---
+++ src/AnnouncementsService.Host.RestfulAPI/Controllers/AnnouncementsController.cs
@@ -0,0 +1,28 @@
+﻿using AnnouncementsService.Domain.Abstractions.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnnouncementsService.Host.RestfulAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AnnouncementsController : ControllerBase
+	{
+		private readonly IAnnouncementsService _announcementsService;
+
+		public AnnouncementsController(IAnnouncementsService announcementsService)
+		{
+			_announcementsService = announcementsService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAllAnnouncements()
+		{
+			try
+			{
+				await _announcementsService.GetAllAnnouncementsAsync();
+			}
+			catch { }
+			return Ok();
+		}
+	}
+}
---
+++ src/AnnouncementsService.Infrastructure.Repositories/AnnouncementsRepository.cs
@@ -0,0 +1,35 @@
+﻿using AnnouncementsService.Domain.Abstractions.Repositories;
+using AnnouncementsService.Domain.Entities;
+using AnnouncementsService.Infrastructure.EfDbContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnnouncementsService.Infrastructure.Repositories
+{
+	public class AnnouncementsRepository(AnnouncementsDbContext dbContext) : IAnnouncementsRepository
+	{
+		private readonly AnnouncementsDbContext _dbContext = dbContext;
+
+		public Task<bool> CreateAsync(Announcement entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> DeleteAsync(Announcement entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<IEnumerable<Announcement>?> GetAllAsync()
+			=> await _dbContext.Announcements.ToArrayAsync();
+
+		public Task<Announcement?> GetAsync(int key)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> UpdateAsync(Announcement entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
