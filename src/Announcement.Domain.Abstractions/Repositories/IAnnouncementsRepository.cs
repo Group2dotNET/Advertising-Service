6942624833d0f3556c7ea987eca28b81a7186ce3
Andrey <kasimovskiy.a.d@gmail.com>
2024-12-02 19:43:04 +0300

    Добавлен общий интерфейс для CRUD операций. Добавлен интерфейс репозитория для объявлений.

---
+++ src/Announcement.Domain.Abstractions/Repositories/IAnnouncementsRepository.cs
@@ -0,0 +1,8 @@
+﻿using AdService.Domain.Entities;
+
+namespace AnnouncementsService.Domain.Abstractions.Repositories;
+
+public interface IAnnouncementsRepository : ICrudRepository<Announcement, int>
+{
+
+}
