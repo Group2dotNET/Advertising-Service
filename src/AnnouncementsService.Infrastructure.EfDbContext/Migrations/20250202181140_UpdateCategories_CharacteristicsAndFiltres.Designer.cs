2baa47627c1183722144fe59f479264a9a19359a
Andrey <kasimovskiy.a.d@gmail.com>
2025-02-02 21:13:34 +0300

    Создание миграции для обновления таблицы категорий

--- src/AnnouncementsService.Infrastructure.EfDbContext/Migrations/20250202181140_UpdateCategories_CharacteristicsAndFiltres.Designer.cs
+++ src/AnnouncementsService.Infrastructure.EfDbContext/Migrations/20250202181140_UpdateCategories_CharacteristicsAndFiltres.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AnnouncementsService.Infrastructure.EfDbContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AnnouncementsService.Infrastructure.EfDbContext.Migrations
 {
     [DbContext(typeof(AnnouncementsDbContext))]
-    partial class AnnouncementsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250202181140_UpdateCategories_CharacteristicsAndFiltres")]
+    partial class UpdateCategories_CharacteristicsAndFiltres
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/AnnouncementsService.Infrastructure.EfDbContext/Migrations/20250202181140_UpdateCategories_CharacteristicsAndFiltres.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnnouncementsService.Infrastructure.EfDbContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCategories_CharacteristicsAndFiltres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "description",
+                table: "categories");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Characteristics",
+                table: "categories",
+                type: "jsonb",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Filtres",
+                table: "categories",
+                type: "jsonb",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Characteristics",
+                table: "categories");
+
+            migrationBuilder.DropColumn(
+                name: "Filtres",
+                table: "categories");
+
+            migrationBuilder.AddColumn<string>(
+                name: "description",
+                table: "categories",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
