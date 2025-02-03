636adb0d21eb9a003e3a4870b775dfde0a7d4e7e
Andrey <kasimovskiy.a.d@gmail.com>
2025-02-03 22:11:35 +0300

    Создание миграции для изменения таблицы категорий с добавлением индекса по столбцу Name

---
+++ src/AnnouncementsService.Infrastructure.EfDbContext/Migrations/20250203190954_UpdateCategories_CreateIndex-Name.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnnouncementsService.Infrastructure.EfDbContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCategories_CreateIndexName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Names_Ascending",
+                table: "categories",
+                column: "name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Names_Ascending",
+                table: "categories");
+        }
+    }
+}
