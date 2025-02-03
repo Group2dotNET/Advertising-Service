c96cdcbb2c62f1256c73da2e74c0e44bf6aad1f9
Andrey <kasimovskiy.a.d@gmail.com>
2025-02-04 00:10:23 +0300

    Реализация методов сервиса для операций над категориями

--- src/AnnouncementsSerivice.Application.Services/CategoriesService.cs
+++ src/AnnouncementsSerivice.Application.Services/CategoriesService.cs
@@ -1,24 +1,93 @@
-﻿using AnnouncementsService.Domain.Abstractions.Repositories;
+﻿using AnnouncementsService.Domain.Abstractions.Dto;
+using AnnouncementsService.Domain.Abstractions.Repositories;
 using AnnouncementsService.Domain.Abstractions.Services;
+using AnnouncementsService.Domain.Entities;
+using System.Diagnostics;
 
 namespace AnnouncementsSerivice.Application.Services;
 
 public class CategoriesService(ICategoriesRepository categoriesRepository) : ICategoriesService
 {
-	public async Task<IEnumerable<CategoryDto>?> GetAllCategories()
-		=> (await categoriesRepository.GetAllAsync())?.Select(c => new CategoryDto { Id = c.Id, CategoryName = c.Name });
+	public async Task<bool> DeleteCategoryAsync(FullCategoryDto category)
+	{
+		try
+		{
+			var categoryEntity = await categoriesRepository.GetCategoryByNameAsync(category.Name);
+			return categoryEntity == null
+				? throw new Exception($"Категории с таким именем не существует [Name={category.Name}]")
+				: await categoriesRepository.DeleteAsync(categoryEntity);
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex.Message);
+			throw;
+		}
+	}
 
-	public async Task<IEnumerable<CategoryDto>?> GetGeneralCategories()
-		=> (await categoriesRepository.GetGeneralCategories())?.Select(c => new CategoryDto { Id = c.Id, CategoryName = c.Name });
+	public async Task<IEnumerable<HierarchyCategoryDto>> GetAllCategoriesHierarchyAsync()
+	{
+		try
+		{
+			var generalCategories = ((await GetGeneralCategoriesAsync())?.Select(c => new HierarchyCategoryDto(c.Name, []))) 
+				?? throw new Exception($"Основные категории отсутствуют");
+			await FillInSubcategoriesAsync(generalCategories);
+			return generalCategories;
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex.Message);
+			throw;
+		}
+	}
 
-	public async Task<CategoryDto?> GetCategoryByName(string categoryName)
-	{ 
+	public async Task<FullCategoryDto?> GetCategoryAsync(string categoryName)
+	{
 		var category = await categoriesRepository.GetCategoryByNameAsync(categoryName);
-		return category is null ? null
-								: new CategoryDto()
-								{
-									Id = category.Id,
-									CategoryName = categoryName,
-								};
+		return category is null
+			? null
+			: new FullCategoryDto(category.Name, category.ParentCategory?.Name ?? "", category.Characteristics ?? "", category.Filtres ?? "");
+	}
+
+	public async Task<IEnumerable<ShortCategoryDto>?> GetGeneralCategoriesAsync()
+		=> (await categoriesRepository.GetGeneralCategories())
+			?.Select(c => new ShortCategoryDto(c.Name));
+
+	public async Task<IEnumerable<ShortCategoryDto>?> GetGeneralSubcategoriesAsync(ShortCategoryDto category)
+		=> (await categoriesRepository.GetSubcategories(category.Name))
+			?.Select(c => new ShortCategoryDto(c.Name));
+
+	public async Task<bool> SaveCategoryAsync(FullCategoryDto savedCategory)
+	{
+		var category = await categoriesRepository.GetCategoryByNameAsync(savedCategory.Name);
+		if (category is null)
+		{
+			var parentCategory = await categoriesRepository.GetCategoryByNameAsync(savedCategory.ParentCategoryName);
+			return await categoriesRepository.CreateAsync(
+												new Category()
+												{
+													Name = savedCategory.Name,
+													ParentCategoryId = parentCategory?.Id,
+													Characteristics = savedCategory.Characteristics,
+													Filtres = savedCategory.Filtres,
+												});
+		}
+		else
+		{
+			return await categoriesRepository.UpdateAsync(category);
+		}
+	}
+
+	private async Task FillInSubcategoriesAsync(IEnumerable<HierarchyCategoryDto> parentCategories)
+	{
+		foreach (HierarchyCategoryDto parentCategory in parentCategories)
+		{
+			var subcategories = (await GetGeneralSubcategoriesAsync(new(parentCategory.Name)))
+				?.Select(s => new HierarchyCategoryDto(s.Name, []));
+			if (subcategories != null)
+			{
+				await FillInSubcategoriesAsync(subcategories);
+				parentCategory.Subcategories.AddRange(subcategories);
+			}
+		}
 	}
 }
