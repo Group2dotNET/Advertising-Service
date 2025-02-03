cb6d95846ef6f3c2d38036d39177ae7db8f8a0f4
Andrey <kasimovskiy.a.d@gmail.com>
2025-02-03 22:12:17 +0300

    Реализация CRUD-репозитория и дополнительных операций для работы с таблицей категорий

--- src/AnnouncementsService.Domain.Abstractions/Repositories/ICategoriesRepository.cs
+++ src/AnnouncementsService.Domain.Abstractions/Repositories/ICategoriesRepository.cs
@@ -2,9 +2,28 @@
 
 namespace AnnouncementsService.Domain.Abstractions.Repositories;
 
+/// <summary>
+/// Репозиторий категорий
+/// </summary>
 public interface ICategoriesRepository : ICrudRepository<Category, int>
 {
+	/// <summary>
+	/// Получить основные категории
+	/// </summary>
+	/// <returns>Список основных категорий</returns>
 	Task<Category[]?> GetGeneralCategories();
 
+	/// <summary>
+	/// Получить категорию по имени
+	/// </summary>
+	/// <param name="name">Имя категории</param>
+	/// <returns>Категория</returns>
 	Task<Category?> GetCategoryByNameAsync(string name);
+
+	/// <summary>
+	/// Получить список дочерних категорий для категории по её имени
+	/// </summary>
+	/// <param name="categoryName">Имя категории</param>
+	/// <returns>Список дочерних категорий</returns>
+	Task<IEnumerable<Category>?> GetSubcategories(string categoryName);
 }
--- src/AnnouncementsService.Infrastructure.Repositories/CategoriesRepository.cs
+++ src/AnnouncementsService.Infrastructure.Repositories/CategoriesRepository.cs
@@ -7,32 +7,41 @@ namespace AnnouncementsService.Infrastructure.Repositories;
 
 public class CategoriesRepository(AnnouncementsDbContext dbContext) : ICategoriesRepository
 {
-	public Task<bool> CreateAsync(Category entity)
+	#region ICrudRepository
+	public async Task<bool> CreateAsync(Category entity)
 	{
-		throw new NotImplementedException();
+		dbContext.Categories.Add(entity);
+		return await dbContext.SaveChangesAsync() > 0;
 	}
 
-	public Task<bool> DeleteAsync(Category entity)
+	public async Task<bool> DeleteAsync(Category entity)
 	{
-		throw new NotImplementedException();
+		dbContext.Remove(entity);
+		return await dbContext.SaveChangesAsync() > 0;
 	}
 
 	public async Task<IEnumerable<Category>?> GetAllAsync()
 		=> await dbContext.Categories.OrderBy(c => c.Name).ToArrayAsync();
 
-	public Task<Category?> GetAsync(int key)
-	{
-		throw new NotImplementedException();
-	}
+	public async Task<Category?> GetAsync(int key)
+		=> await dbContext.Categories.FirstOrDefaultAsync(c => c.Id == key);
 
-	public Task<bool> UpdateAsync(Category entity)
+	public async Task<bool> UpdateAsync(Category entity)
 	{
-		throw new NotImplementedException();
+		dbContext.Update(entity);
+		return await dbContext.SaveChangesAsync() > 0;
 	}
+	#endregion
 
 	public async Task<Category[]?> GetGeneralCategories()
 		=> await dbContext.Categories.Where(c => c.ParentCategory == null).ToArrayAsync();
 
 	public async Task<Category?> GetCategoryByNameAsync(string name)
 		=> await dbContext.Categories.AsNoTracking().SingleOrDefaultAsync(c => c.Name == name);
+
+	public async Task<IEnumerable<Category>?> GetSubcategories(string categoryName)
+	{
+		var category = await dbContext.Categories.AsNoTracking().SingleAsync(c => c.Name == categoryName);
+		return category.ChildCategories;
+	}
 }
