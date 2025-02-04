8d1d2a4244a3bc38e041b8433e46b293f8d8c856
Andrey <kasimovskiy.a.d@gmail.com>
2025-02-04 19:55:43 +0300

    Создание контроллера для категорий

---
+++ src/AnnouncementsService.Host.RestfulAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,11 @@
+﻿using AnnouncementsService.Domain.Abstractions.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnnouncementsService.Host.RestfulAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CategoriesController(ICategoriesService categoriesService) : ControllerBase
+{
+	
+}
